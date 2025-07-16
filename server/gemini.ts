6665e467a1eef6c3c7c9f49cdbdcb9c34b527c3f
Pratonic <pratyushbhatia004@gmail.com>
2025-07-16 22:07:06 +0530

    Update gemini.ts

--- server/gemini.ts
+++ server/gemini.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import { GoogleGenAI } from "@google/genai";
 
 // This API key is from Gemini Developer API Key, not vertex AI API Key
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || "" });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || "AIzaSyDC6lLjZSVxwdliHrQBjk4YvZT4jqtKThw" });
 
 export interface WasteAnalysisResult {
   wasteType: string;
